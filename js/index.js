3f9b579e44b2dddd1e4985bf2c8358b62b388448
unknown <Dieki@.(none)>
2011-04-22 16:21:46 -0500

    Do the slides one at a time

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
 window.onload = function(){
-	$("#links").slideDown();
+	$("#portfolio-bar").slideDown();
+	$("#blog-bar").delay(1000).slideDown();
+	$("#resources-bar").delay(2000).slideDown();
 };
