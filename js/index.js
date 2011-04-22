01780eab6a4ee8328bfd19d95bc09e71e77c3e71
unknown <Dieki@.(none)>
2011-04-22 18:09:25 -0500

    Misc. Changes

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
-window.onload = function(){
-	$("#portfolio-bar").slideDown();
-	$("#blog-bar").delay(250).slideDown();
-	$("#resources-bar").delay(400).slideDown();
+window.onload = function(){	
+	$("#portfolio-bar").animate({opacity:1, marginLeft:0}, 300, 'easeInQuad');
+	$("#blog-bar").delay(200).animate({opacity:1, marginLeft:0}, 300, 'easeInQuad');
+	$("#resources-bar").delay(400).animate({opacity:1, marginLeft:0}, 300, 'easeInQuad');
 };
