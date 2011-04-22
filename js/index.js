4233c57a994181095e18fdd3fe924e02d18e7c63
unknown <Dieki@.(none)>
2011-04-22 16:28:46 -0500

    Tweak timing

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 window.onload = function(){
 	$("#portfolio-bar").slideDown();
-	$("#blog-bar").delay(500).slideDown();
-	$("#resources-bar").delay(750).slideDown();
+	$("#blog-bar").delay(250).slideDown();
+	$("#resources-bar").delay(400).slideDown();
 };
