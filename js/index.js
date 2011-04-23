9feae1c969be9dc045c3a05eed214fa4934982bb
unknown <Dieki@.(none)>
2011-04-22 20:52:59 -0500

    Mess with loading screen

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 window.onload = function(){
-	$("#loading").hide();	
+	$("#loading").fadeOut();	
 	$("#portfolio-bar").animate({opacity:1, marginLeft:0}, 300, 'easeInQuad');
 	$("#blog-bar").delay(200).animate({opacity:1, marginLeft:0}, 300, 'easeInQuad');
 	$("#resources-bar").delay(400).animate({opacity:1, marginLeft:0}, 300, 'easeInQuad');
