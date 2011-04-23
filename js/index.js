599f54d45616f9f4ded4191ab7e05d19315f9347
unknown <Dieki@.(none)>
2011-04-23 18:55:04 -0500

    Start converting to JPEG+transparency

--- js/index.js
+++ js/index.js
@@ -1,9 +1,6 @@
 window.onload = function(){
 	$("#loading").fadeOut();
-	drawLinksCanvas();
-	$("#portfolio-bar").delay(200).animate({opacity:1, marginLeft:0}, 300, 'easeInQuad');
-	$("#blog-bar").delay(400).animate({opacity:1, marginLeft:0}, 300, 'easeInQuad');
-	$("#resources-bar").delay(600).animate({opacity:1, marginLeft:0}, 300, 'easeInQuad');
+
 };
 
 function drawLinksCanvas(){
