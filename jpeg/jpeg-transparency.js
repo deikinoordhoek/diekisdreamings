f27cfeb07403436a5044dffa5c98497ac6cb9a4e
unknown <Dieki@.(none)>
2011-04-23 13:10:26 -0500

    Add a folder full of experimental stuff

---
+++ jpeg/jpeg-transparency.js
@@ -0,0 +1,61 @@
+window.onload = function(){
+	images = getImageElements();
+	canvases = addCanvasElements(images);
+	drawCanvasElements(canvases, images);
+}
+
+function getImageElements(){
+	images = new Array();
+	num_images = 0;
+	all_images = document.getElementsByTagName("img");
+	for (i = 0; i < all_images.length; i++){
+		if (all_images[i].getAttribute("data-image") && all_images[i].getAttribute("data-mask")){
+			images[num_images] = all_images[i];
+			num_images++;
+		}
+	}
+	return images;
+}
+
+function addCanvasElements(images){
+	canvas_elements = new Array();
+	for (i = 0; i < images.length; i++){
+		canvas_element = document.createElement('canvas');
+		canvas_elements[i] = canvas_element;
+		images[i].parentNode.insertBefore(canvas_elements[i], images[i]);
+	}
+	return canvas_elements;
+}
+
+function drawCanvasElements(canvas_elements, images){
+	mask = new Array()
+	image = new Array()
+	for (i = 0; i < canvas_elements.length;i++){
+		mask[i] = new Image()
+		image[i] = new Image()
+		mask[i].src = images[i].getAttribute('data-mask');
+		image[i].src = images[i].getAttribute('data-image');
+		mask[i].onload = drawImage(i, mask[i], image[i], canvas_elements[i]);
+	}
+}
+
+function drawImage(i_num, mask, image, canvas){
+	// Remember, only mask is loaded at this point.
+	context = canvas.getContext('2d');
+	canvas.width = mask.width;
+	canvas.height = mask.height;
+	context.drawImage(mask, 0, 0);	
+	imgd = context.getImageData(0, 0, mask.width, mask.height);
+	pix_mask = imgd.data;
+	image.onload = function(){
+		context.drawImage(this,0,0)
+		imgd = context.getImageData(0,0,mask.width, mask.height);
+		pix = imgd.data;
+		for (var i=0; i < pix.length; i +=4){
+			pix[i + 3] = pix_mask[i];
+		}
+		context.putImageData(imgd, 0, 0);
+	}
+	
+	
+}
--- js/index.js
+++ js/index.js
@@ -1,6 +1,16 @@
 window.onload = function(){
-	$("#loading").fadeOut();	
+	$("#loading").fadeOut();
+	drawLinksCanvas();
 	$("#portfolio-bar").delay(200).animate({opacity:1, marginLeft:0}, 300, 'easeInQuad');
 	$("#blog-bar").delay(400).animate({opacity:1, marginLeft:0}, 300, 'easeInQuad');
 	$("#resources-bar").delay(600).animate({opacity:1, marginLeft:0}, 300, 'easeInQuad');
 };
+
+function drawLinksCanvas(){
+	canvas = document.getElementById("links-canvas");
+	context = canvas.getContext('2d');
+	links_image = new Image();
+	links_image.src = "../images/links.jpg"
+	links_mask = new Image();
+	links_mask.src = "../images/links_mask.png"
+}
