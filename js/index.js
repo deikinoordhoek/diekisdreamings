9b0a5a58ff609dc217d20570eb7a66ec2bee2d48
unknown <Dieki@.(none)>
2011-04-25 13:14:37 -0500

    Move to DOMContentLoaded and fix the weird corruption issues. Yay for version control!
    Signed-off-by: unknown <Dieki@.(none)>

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-window.onload = function(){
+window.addEventListener("DOMContentLoaded",function(){
 	if (window.innerWidth >= 1920) {
 		document.getElementById("links").innerHTML = '<div id="links-left"> <a href="portfolio"><img data-fallback="images/portfolio-bar.png" data-image="images/portfolio-bars.jpg" data-mask="images/portfolio-mask.png" id="portfolio-bar" class="bar-image" data-height="184"></a><img data-fallback="images/blank-banner.png" data-image="images/blank-banner-image.png" data-mask="images/blank-banner-mask.png" class="bar-image"><a href="resources"><img data-fallback="images/resources-bar.png" data-image="images/portfolio-bars.jpg" data-mask="images/portfolio-mask.png" id="portfolio-bar" class="bar-image" data-height="184" data-offset-y="368"></a></div><div id="links-right"><img data-fallback="images/blank-banner.png" data-image="images/blank-banner-image.png" data-mask="images/blank-banner-mask.png" class="bar-image">						<a href="http://blog.diekisdreamings.com"><img data-fallback="images/blog-bar.png" data-image="images/portfolio-bars.jpg" data-mask="images/portfolio-mask.png" id="portfolio-bar" class="bar-image" data-height="184" data-offset-y="184"></a><img data-fallback="images/blank-banner.png" data-image="images/blank-banner-image.png" data-mask="images/blank-banner-mask.png" class="bar-image"></div><div class="clear"></div>'
 	}
@@ -13,7 +13,7 @@ window.onload = function(){
 		document.getElementById("links").style.marginRight = "120px";
 	}
 	document.getElementById('email-link').href="mailto:" + retrieveEmail();
-}
+}, false);
 
 function retrieveEmail(){
 	return 'qvrxv@qvrxvfqernzvatf.pbz'.replace(/[a-zA-Z]/g,function(c){return String.fromCharCode((c<="Z"?90:122)>=(c=c.charCodeAt(0)+13)?c:c-26);});
