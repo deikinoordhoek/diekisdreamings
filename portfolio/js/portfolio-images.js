95e0e6785a35007ae22602367ebfc0117e9a41a1
unknown <Dieki@.(none)>
2011-05-03 11:37:51 -0500

    Update with changes.

--- portfolio/js/portfolio-images.js
+++ portfolio/js/portfolio-images.js
@@ -1,3 +1,4 @@
+var inner_glow_data;
 function getPortfolioManifest(manifest_file){
 	var req = new XMLHttpRequest();
 	req.open('GET', manifest_file, false)
@@ -6,12 +7,18 @@ function getPortfolioManifest(manifest_file){
 }
 
 function getImageData(input_xml){
-	parser = new DOMParser();
-	xmlDoc = parser.parseFromString(input_xml,"text/xml")
+	if (window.DOMParser){
+		parser = new DOMParser();
+		xmlDoc = parser.parseFromString(input_xml,"text/xml")
+	} else {
+		xmlDoc = new ActiveXObject("Microsoft.XMLDOM");
+		xmlDoc.async = "false";
+		xmlDoc.loadXML(input_xml);
+	}
 	image_data = new Array()
 	for (i=0;i < xmlDoc.getElementsByTagName("image").length; i++){	
 
-		ie_thumbnail_url = xmlDoc.getElementsByTagName("image")[i].getElementsByTagName("thumbnail")[0].getElementsByTagName("url")[0].childNodes[0].nodeValue;
+		ie_thumbnail_url = "portfolio/images/" + xmlDoc.getElementsByTagName("image")[i].getElementsByTagName("thumbnail")[0].getElementsByTagName("url")[0].childNodes[0].nodeValue;
 		thumbnail_url = xmlDoc.getElementsByTagName("image")[i].getElementsByTagName("thumbnail")[0].getElementsByTagName("plain_url")[0].childNodes[0].nodeValue;
 		fullsize_url = xmlDoc.getElementsByTagName("image")[i].getElementsByTagName("fullsize")[0].getElementsByTagName("url")[0].childNodes[0].nodeValue;
 		image_title = xmlDoc.getElementsByTagName("image")[i].getElementsByTagName("image_title")[0].childNodes[0].nodeValue;
@@ -39,37 +46,63 @@ function loadPortfolio(manifest_file){
 	}
 	return portfolio_data;
 }
-
+function prerenderInnerGlow(){
+	var ITEM_OFFSET = 9;
+	var ITEM_WIDTH = 662;
+	var ITEM_HIEGHT = 162;
+	var ITEM_INNER_GLOW_RADIUS = 46;
+	canvas = document.createElement("canvas");
+	context = canvas.getContext("2d");
+	canvas.width = 680;
+	canvas.height = 180;
+	context.rectWithRoundRectHole(0,0, context.canvas.width, context.canvas.height, ITEM_OFFSET + .5, ITEM_OFFSET + .5,ITEM_WIDTH - .5 + ITEM_OFFSET,ITEM_HIEGHT - .5 + ITEM_OFFSET,5);
+	context.fillStyle = "white";
+	context.fill();
+	stackBlurCanvasRGBA(context.canvas, 0, 0, context.canvas.width, context.canvas.height, ITEM_INNER_GLOW_RADIUS);
+	inner_glow_data = new Image()
+	inner_glow_data.src = context.canvas.toDataURL("image/png")
+}
 function drawOnCanvas(canvas, highlight) {
 	context = canvas.getContext('2d');
 	canvas.width = 680;
 	canvas.height = 180;
 	i = this.image;
 	i.src = "portfolio/images/" + this.plain_url;
-	i.onload = constructPortfolioDisplay(this, context, highlight);
+	if (i.width == 0) {
+		i.onload = constructPortfolioDisplay(this, context, highlight, i);
+	}
+	else{
+		i_c = constructPortfolioDisplay(this, context, highlight, i)
+		i_c();
+	}
 	
 
 }
-function constructPortfolioDisplay(parent, context, highlight){
+function constructPortfolioDisplay(parent, context, highlight, image){
 	return function(){
 		var ITEM_OFFSET = 9;
 		var ITEM_WIDTH = 662;
 		var ITEM_HIEGHT = 162;
-		var image_offset_x = 0;
-		var image_offset_y = 0;
-		var image_scale = 1;
+		var ITEM_INNER_GLOW_RADIUS = 46;
+		var DO_INNER_GLOW = true;
+
+		image_offset_x = 0;
+		image_offset_y = 0;
+		image_scale = 1;
 		if (!!parent.topX){
 			image_offset_x = parent.topX;
 			image_offset_y = parent.topY;
 			image_scale =  ITEM_WIDTH / parent.width;
 		}
+
+		//Draw the shadow
 		context.save();
 			context.roundRect(ITEM_OFFSET, ITEM_OFFSET, ITEM_WIDTH + ITEM_OFFSET, ITEM_HIEGHT + ITEM_OFFSET,5);
 			context.fillStyle = "black";
 			context.shadowColor = "black";
 			context.shadowOffsetX = 0;
 			context.shadowOffsetY = 0;
-			context.shadowBlur = 14;
+			context.shadowBlur = 10;
 			context.fill();
 		context.restore();
 
@@ -83,27 +116,40 @@ function constructPortfolioDisplay(parent, context, highlight){
 			context.save();
 			context.clip();
 			if (!!parent.topX){
-				context.drawImage(this,image_offset_x, image_offset_y, parent.width, parent.height, ITEM_OFFSET,50 + ITEM_OFFSET, ITEM_WIDTH, 112);
+				context.drawImage(image,image_offset_x, image_offset_y, parent.width, parent.height, ITEM_OFFSET,50 + ITEM_OFFSET, ITEM_WIDTH, 112);
 			}
 			else {
-				context.drawImage(this, ITEM_OFFSET,50 + ITEM_OFFSET);				
+				context.drawImage(image, ITEM_OFFSET,50 + ITEM_OFFSET);				
 			}
-	
+			//Draw the highlight
+			if (highlight == true){
+				context.save();
+					highlight_gradient = context.createRadialGradient(ITEM_WIDTH / 2, ITEM_HIEGHT / 2, 0, ITEM_WIDTH / 2, ITEM_HIEGHT / 2, ITEM_WIDTH * .4);
+					highlight_gradient.addColorStop(0,  "rgba(128,128,255,.4)");
+					highlight_gradient.addColorStop(1,  "rgba(255,255,255,0)");
+					context.roundRect(ITEM_OFFSET - .5, ITEM_OFFSET - .5,ITEM_WIDTH + .5 + ITEM_OFFSET,ITEM_HIEGHT + .5 + ITEM_OFFSET,5);
+					context.globalCompositeOperation = "lighter";
+					context.fillStyle = highlight_gradient;
+					context.fill();
+				context.restore();
+			}	
 			//Draw the top rectangular box w/gradient.
 			text_box_gradient = context.createLinearGradient(0,9,0,54);
-			text_box_gradient.addColorStop(0, "#383838");
-			text_box_gradient.addColorStop(1, "#2b2b2b");
+			text_box_gradient.addColorStop(0, "#333333");
+			text_box_gradient.addColorStop(1, "#333333");
 	
 			context.fillStyle = text_box_gradient;
 			context.fillRect(ITEM_OFFSET, ITEM_OFFSET,ITEM_WIDTH,54);
 		
 		
 			//Draw the shadow for top rectangular box
+
 			box_shadow_gradient = context.createLinearGradient(0,52 + ITEM_OFFSET,0, 61 + ITEM_OFFSET);
 			box_shadow_gradient.addColorStop(0, "rgba(0,0,0,.6)");
 			box_shadow_gradient.addColorStop(1, "transparent");
 			context.fillStyle = box_shadow_gradient;
 			context.fillRect(ITEM_OFFSET,54 + ITEM_OFFSET,662, 9);
+
 		context.restore();
 
 
@@ -118,23 +164,37 @@ function constructPortfolioDisplay(parent, context, highlight){
 				context.shadowOffsetX = 2;
 				context.shadowOffsetY = 1;
 				context.shadowBlur = 4;
-				context.fillText(parent.title, ITEM_WIDTH / 2, 69);
+				context.fillText(parent.title, ITEM_WIDTH / 2 + ITEM_OFFSET, 60 + ITEM_OFFSET);
 			context.restore();
 
 			text_gradient = context.createLinearGradient(0,15,0, 60);
 			text_gradient.addColorStop(0, "#f9f9f9");
 			text_gradient.addColorStop(1, "#bebebe");
 			context.fillStyle= text_gradient;
-			context.fillText(parent.title, ITEM_WIDTH / 2, 69);
+			context.fillText(parent.title, ITEM_WIDTH / 2 + ITEM_OFFSET, 60 + ITEM_OFFSET);
 		context.restore()
+
+		//Put the inner glow
+		if (DO_INNER_GLOW){
+			context.save();
+				context.roundRect(ITEM_OFFSET, ITEM_OFFSET, ITEM_WIDTH + ITEM_OFFSET, ITEM_HIEGHT + ITEM_OFFSET,5);
+				context.clip();
+				context.globalAlpha = .18;
+				context.drawImage(inner_glow_data, 0, 0);
+			context.restore()
+		}
 	
 		//Draw the stroke around the outside.
+
 		context.save();
-			context.roundRect(ITEM_OFFSET - .5, ITEM_OFFSET - .5,ITEM_WIDTH + .5 + ITEM_OFFSET,ITEM_HIEGHT + .5 + ITEM_OFFSET,5);
-			context.strokeStyle = "rgba(255,255,255,.48);";
+			context.roundRect(ITEM_OFFSET + .5, ITEM_OFFSET + .5,ITEM_WIDTH - .5 + ITEM_OFFSET,ITEM_HIEGHT - .5 + ITEM_OFFSET,5);
+			context.strokeStyle = "#888";
 			context.lineWidth   = 1;
 			context.stroke();
 		context.restore();
+
+
+
 	}
 }
 CanvasRenderingContext2D.prototype.roundRect = function(sx,sy,ex,ey,r) {
@@ -151,5 +211,30 @@ CanvasRenderingContext2D.prototype.roundRect = function(sx,sy,ex,ey,r) {
 	this.arc(sx+r,ey-r,r,r2d*90,r2d*180,false);
 	this.lineTo(sx,sy+r);
 	this.arc(sx+r,sy+r,r,r2d*180,r2d*270,false);
+	this.closePath();
+}
+
+CanvasRenderingContext2D.prototype.rectWithRoundRectHole = function(sx,sy,ex,ey, sx2,sy2,ex2,ey2,r) {
+	var r2d = Math.PI/180;
+	if( ( ex - sx ) - ( 2 * r ) < 0 ) { r = ( ( ex - sx ) / 2 ); } //ensure that the radius isn't too large for x
+	if( ( ey - sy ) - ( 2 * r ) < 0 ) { r = ( ( ey - sy ) / 2 ); } //ensure that the radius isn't too large for y
+
+	this.beginPath();
+
+	this.moveTo(sx2,sy2);
+	this.lineTo(sx2, ey2);
+	this.lineTo(ex2, ey2);
+	this.lineTo(ex2, sy2);
+ 
+	this.moveTo(sx+r,sy);
+	this.lineTo(ex-r,sy);
+	this.arc(ex-r,sy+r,r,r2d*270,r2d*360,false);
+	this.lineTo(ex,ey-r);
+	this.arc(ex-r,ey-r,r,r2d*0,r2d*90,false);
+	this.lineTo(sx+r,ey);
+	this.arc(sx+r,ey-r,r,r2d*90,r2d*180,false);
+	this.lineTo(sx,sy+r);
+	this.arc(sx+r,sy+r,r,r2d*180,r2d*270,false);
+
 	this.closePath();
 }
--- portfolio/js/portfolio.js
+++ portfolio/js/portfolio.js
@@ -1,29 +1,78 @@
 
 window.onload = displayImages;
+var MAIN_PAGE = 0;
+var FANTASY_PAGE = 1;
+var SCIFI_PAGE = 2;
+var current_page = MAIN_PAGE;
 
 
 function formatTitle(title, currentArray, currentIndex, currentOpts) {
     return '<div class="images-title">' + (title && title.length ? '<b>' + title + '</b>' : '' ) + '</div>';
 }
+$(function(){
+	$('#fantasy-link').click(function(){
+		showFantasy();
+	});
+	$('#scifi-link').click(function(){
+		showScifi();
+	});
+	$.History.bind('/fantasy', showFantasy);
+	$.History.bind('/scifi', showScifi);
+	$.History.bind('/main', showMain);
+
+});
 var fantasy_data;
 function displayImages(){
-	$("#fantasy-banner").delay(100).fadeIn();
-	$("#scifi-banner").delay(200).fadeIn();
+	//$("#fantasy-banner").delay(100).fadeIn();
+	//$("#scifi-banner").delay(200).fadeIn();
 	fantasy_data = loadPortfolio("portfolio/images/fantasy.xml");
 	fantasy_div = document.getElementById("fantasy-images")
+	prerenderInnerGlow();
 	for (n = 0; n < fantasy_data.length; n++){
 		a = document.createElement('a');
 		a.href = fantasy_data[n].fullsize.image.src;
 		a.setAttribute("class", "portfolio-image");
 		a.setAttribute("title", fantasy_data[n].image_title);
 		a.setAttribute("onclick", "_gaq.push(['_trackPageview'," + fantasy_data[n].fullsize.image.src + ']);');
-
-		c = document.createElement('canvas');
-		c.setAttribute("class", "portfolio-image");
-		c.setAttribute("data-num", n);
-		a.appendChild(c);
-		fantasy_data[n].thumbnail.drawOnCanvas(c);
+		if (Modernizr.canvas) {
+			c = document.createElement('canvas');
+			c.setAttribute("class", "portfolio-image");
+			c.setAttribute("onmouseover", "fantasy_data[" + n + "].thumbnail.drawOnCanvas(this, true);");
+			c.setAttribute("onmouseout", "fantasy_data[" + n + "].thumbnail.drawOnCanvas(this, false);");
+			a.appendChild(c);
+			fantasy_data[n].thumbnail.drawOnCanvas(c);
+		} else {
+			i = new Image();
+			i.setAttribute("class", "portfolio-image");
+			i.src = fantasy_data[n].thumbnail.ie_url;
+			a.appendChild(i);
+		}
 		fantasy_div.appendChild(a);
+	}
+
+	scifi_data = loadPortfolio("portfolio/images/sci-fi.xml");
+	scifi_div = document.getElementById("scifi-images")
+
+	for (n = 0; n < scifi_data.length; n++){
+		a = document.createElement('a');
+		a.href = scifi_data[n].fullsize.image.src;
+		a.setAttribute("class", "portfolio-image");
+		a.setAttribute("title", scifi_data[n].image_title);
+		a.setAttribute("onclick", "_gaq.push(['_trackPageview'," + scifi_data[n].fullsize.image.src + ']);');
+		if (Modernizr.canvas) {
+			c = document.createElement('canvas');
+			c.setAttribute("class", "portfolio-image");
+			c.setAttribute("onmouseover", "scifi_data[" + n + "].thumbnail.drawOnCanvas(this, true);");
+			c.setAttribute("onmouseout", "scifi_data[" + n + "].thumbnail.drawOnCanvas(this, false);");
+			a.appendChild(c);
+			scifi_data[n].thumbnail.drawOnCanvas(c);
+		} else {
+			i = new Image();
+			i.setAttribute("class", "portfolio-image");
+			i.src = scifi_data[n].thumbnail.ie_url;
+			a.appendChild(i);
+		}
+		scifi_div.appendChild(a);
 	}
 	$("a.portfolio-image").fancybox({
 		'transitionIn'	     :	'fade',
@@ -40,8 +89,35 @@ function displayImages(){
 }
 
 function showFantasy(){
-	$("#fantasy-banner").fadeOut();
-	$("#scifi-banner").fadeOut();
-	$("#fantasy-images").delay(500).fadeIn();
+	if (current_page == MAIN_PAGE) {
+		$("#fantasy-banner").fadeOut();
+		$("#scifi-banner").fadeOut();
+		$("#fantasy-images").delay(500).fadeIn();
+	} else if (current_page == SCIFI_PAGE) {
+		$("#scifi-image").fadeOut();
+		$("#fantasy-images").delay(500).fadeIn();
+	}
+	current_page = FANTASY_PAGE;
 }
 
+function showScifi(){
+	if (current_page == MAIN_PAGE) {
+		$("#fantasy-banner").fadeOut();
+		$("#scifi-banner").fadeOut();
+		$("#scifi-images").delay(500).fadeIn();
+	} else if (current_page == FANTASY_PAGE) {
+		$("#fantasy-images").fadeOut();
+		$("#scifi-images").delay(500).fadeIn();
+	}
+	current_page = SCIFI_PAGE;
+
+}
+
+function showMain(){
+	$("#fantasy-images").fadeOut();
+	$("#scifi-images").fadeOut();
+	$("#fantasy-banner").delay(500).fadeIn();
+	$("#scifi-banner").delay(500).fadeIn();
+	current_page = MAIN_PAGE;
+
+}
